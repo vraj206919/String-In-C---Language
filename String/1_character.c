09b1b40fa67a23b741857a335ede3a72c397d01e
vraj206919 <vrajd9313@gmail.com>
2026-03-18 14:20:07 +0530

    Add files via upload

---
+++ String/1_character.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+
+int main()
+{
+    char letter = 'A';
+    printf("The character is: %c\n", letter);
+    printf("The ASCII value is: %d\n", letter);
+
+    char LastLetter = 'Z';
+    printf("The character is: %c\n", LastLetter);
+    printf("The ASCII value is: %d\n", LastLetter);
+
+    char lowercase = 'a';
+    printf("The character is: %c\n", lowercase);
+    printf("The ASCII value is: %d\n", lowercase);
+
+    char LastLowercase = 'z';
+    printf("The character is: %c\n", LastLowercase);
+    printf("The ASCII value is: %d\n", LastLowercase);
+
+    return 0;
+}
+
+
